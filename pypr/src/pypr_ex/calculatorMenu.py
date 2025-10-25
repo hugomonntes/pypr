c19598b2446236f1a79b65a9f178c82a8fdd8f6a
hugomonntes <huugomontees@gmail.com>
2025-10-25 09:16:46 +0200

    Add calculatorMenu

--- pypr/src/pypr_ex/calculatorMenu.py
+++ pypr/src/pypr_ex/calculatorMenu.py
@@ -3,10 +3,15 @@ def getData():
 
 def showData():
     print(getData())
-    
+
+def isHigher(num1, num2):
+    if num1 >= num2:
+        return True
+    return False
+
 def calculateDivision():
     num1 = getData()
     num2 = getData()
-    print(f"La división es: {float(num1) / float(num2)}")
-    print(f"Cociente: {float(num1) // float(num2)}")
-    print(f"Resto: {float(num1) % float(num2)}")
+    print(f"Cociente: {num1 / num2}") if isHigher(num1, num2) else print(f"Cociente: {num2 / num1}")
+    print(f"Resto: {num1 % num2}") if isHigher(num1, num2) else print(f"Resto: {num2 % num1}")
+    print()
