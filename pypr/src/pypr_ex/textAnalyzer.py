4cb4fd0e38d0bdde8af92b5106c7ce333c98f813
hugomonntes <huugomontees@gmail.com>
2025-10-25 13:39:05 +0200

    Add textAnalyzer

--- pypr/src/pypr_ex/textAnalyzer.py
+++ pypr/src/pypr_ex/textAnalyzer.py
@@ -1,5 +1,10 @@
 def count_words(text):
+    # text = "El gato negro y el gato blanco juegan. El gato negro es rápido."
     words = text.split(" ")
+    for i in range (1, words.count() + 1):
+        if words.index(i).isalpha():
+            words.remove(i)
     print(words)
+        
     
-count_words("hola que . tal")
+count_words("El gato negro y el gato blanco juegan. El gato negro es rápido.")
