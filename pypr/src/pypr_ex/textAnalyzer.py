4a0030c6af4bed968ff23f042e81ffb31aedba69
hugomonntes <huugomontees@gmail.com>
2025-10-25 13:51:43 +0200

    Add textAnalyzer

--- pypr/src/pypr_ex/textAnalyzer.py
+++ pypr/src/pypr_ex/textAnalyzer.py
@@ -1,10 +1,16 @@
-def count_words(text):
-    # text = "El gato negro y el gato blanco juegan. El gato negro es rápido."
+import string;
+def count_words():
+    text = "El gato negro y el gato blanco juegan. El gato negro es rápido."
+    counter_words = 0
     words = text.split(" ")
-    for i in range (1, words.count() + 1):
-        if words.index(i).isalpha():
-            words.remove(i)
-    print(words)
+    checkedWords = []
+    for word in words:
+        wordFormated = word.strip(string.punctuation) #Quito los puntos y simbolos en caso de que esten pegados a la palabra
+        if wordFormated.isalpha():
+            checkedWords.append(wordFormated)
+            if word in words: #FIXME contar cada aparicion de la palabra mas aparecida
+                counter_words += 1
+    print(checkedWords, counter_words)
         
     
-count_words("El gato negro y el gato blanco juegan. El gato negro es rápido.")
+count_words()
