bc6f63c04cfab19563d71e39b622d5065a056694
hugomonntes <huugomontees@gmail.com>
2025-11-02 00:08:56 +0100

    Not fix lowercase

--- pypr/src/pypr_ex/textAnalyzer.py
+++ pypr/src/pypr_ex/textAnalyzer.py
@@ -1,13 +1,12 @@
 import string;
 def count_words():
     text = "El gato negro y el gato blanco juegan. El gato negro es rápido."
-    counter_words = 0
     words = text.split(" ")
     checkedWords = []
     for word in words:
-        wordFormated = word.strip(string.punctuation) #Quito los puntos y simbolos en caso de que esten pegados a la palabra
+        wordFormated = word.strip(string.punctuation) #Quito los puntos y simbolos en caso de que esten pegados a la palabra y lo pongo en minuscula
         if wordFormated.isalpha():
-            checkedWords.append(wordFormated.capitalize)
+            checkedWords.append(wordFormated)
     
     wordPerAppearances = {}
     for word in checkedWords:
