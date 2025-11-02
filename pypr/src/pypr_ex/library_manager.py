e403e66d518761ff7c42eef6f3278590befd0dad
hugomonntes <huugomontees@gmail.com>
2025-11-02 10:57:45 +0100

    Add fn validate isbn

--- pypr/src/pypr_ex/library_manager.py
+++ pypr/src/pypr_ex/library_manager.py
@@ -1,3 +1,6 @@
+import string
+
+
 def getPositiveNumber():
     while True:
         try:
@@ -9,7 +12,18 @@ def getPositiveNumber():
         except ValueError:
             print("Número no válido")
         
-getPositiveNumber()
+# getPositiveNumber()
+
+def isValidIsbn(isbn: str):
+    isbn = isbn.replace(" ", "").replace("_", "")
+    if len(isbn) != 10 or not isbn[:9].isdigit():
+        return False
+    
+    if isbn[-1].isdigit() or isbn[-1] == "X":
+        return True
+
+    return False
+print(isValidIsbn("987_987_98_8X"))
 
 # b) Realiza un función comprueba_isbn a la cual se le pasa un string y hace una
 # comprobación sencilla del isbn de 10 digitos teniendo en cuenta las siguientes
